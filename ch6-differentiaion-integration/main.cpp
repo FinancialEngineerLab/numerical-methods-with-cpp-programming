08ba407072933d01147966f411144e8ae7e6ae93
FineFinance <42334069+FinancialEngineerLab@users.noreply.github.com>
2023-09-11 13:12:28 +0900

    Create main.cpp

---
+++ ch6-differentiaion-integration/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+#define f(x) (exp(-pow(x,3))))
+
+using namespace std;
+
+int main()
+{
+    int i;
+    int n;
+    float a;
+    float b;
+    float x[30];
+    float y[30];
+    
+    clrscr();
+    
+    cin >> a>> b;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    
+    cin >> n;
+    
+    h= (b-a)/n;
+    
+    
+    
+}
